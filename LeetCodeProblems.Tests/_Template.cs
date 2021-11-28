f93d454fd501bf06f90c64b7990698998b36bc11
Nazar Kalashnikov <nazar.kalashnikov@gmail.com>
2021-11-29 00:20:28 +0200

    Add template for problems

---
+++ LeetCodeProblems.Tests/_Template.cs
@@ -0,0 +1,22 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeetCodeProblems.Tests
+{
+    public class LT0000_Tests
+    {
+        // #0 https://leetcode.com/problems/xxx
+        [Test]
+        public void DoSmthTest(int x, bool expectedResult)
+        {
+            bool result = DoSmth(x);
+
+            result.Should().Be(expectedResult);
+        }
+
+        static public bool DoSmth(int x)
+        {
+            return false;
+        }
+    }
+}
